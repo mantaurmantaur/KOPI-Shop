15f0fcaabb3967cc28a18a7a44c062fe764690b3
mantaurmantaur <orng.digital@gmail.com>
2026-01-04 19:02:24 +0800

    used render alone
    might be changed for the future but this will do for now, means no  error because good ass code is making some errors now ey?

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,18 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
+<<<<<<< Updated upstream
   const router = useRouter();
 
   useEffect(() => {
     router.push("/login");
   }, [router]);
+=======
+  // const { data } = await supabase.auth.getSession();
+  // if (data.session) {
+  //   redirect("/login");
+  // }
+
+  redirect("/login");
+>>>>>>> Stashed changes
 }
